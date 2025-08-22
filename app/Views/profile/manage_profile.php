240b33c96c2c087c4d3ffc5d6a53e4974c6f0792
shandyka24 <sandykatribuana@gmail.com>
2025-08-22 22:07:16 +0700

    tes

--- app/Views/profile/manage_profile.php
+++ app/Views/profile/manage_profile.php
@@ -50,8 +50,6 @@ body, .card, .table, .btn, .form-control, .form-select, .modal, .form-label, lab
     font-size: 18px;
 }
 
-/* tes */
-
 /* DataTable specific styles */
 .dataTables_wrapper, .dataTables_filter input, .dataTables_length select {
     font-size: 20px;
